87a5576fd3f57241bcd433615a1bbec3b401000a
Ramazan <laumanov.r@gmail.com>
2019-06-03 18:39:07 +0600

    added routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Weather from "@/components/weather/Weather.vue";
+import DailyForecast from "@/components/daily/DailyForecast.vue";
 
 Vue.use(Router);
 
@@ -10,17 +11,13 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "weather",
+      component: Weather
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/daily/:city",
+      name: "daily",
+      component: DailyForecast
     }
   ]
 });
