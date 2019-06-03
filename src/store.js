876385887fb216104623c71b650c7014fa88bada
Ramazan <laumanov.r@gmail.com>
2019-06-03 18:39:31 +0600

    added vuex store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    weather: {}
+  },
+  mutations: {
+    setWeatherForecast(state, response) {
+      state.weather = response.data;
+    }
+  },
+  actions: {
+    async getWeatherForecast({ commit }, city) {
+      try {
+        const response = await axios.get(`http://localhost:3000/${city}`);
+        commit("setWeatherForecast", response);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+  getters: {
+    showCurrentForecast(state) {
+      return state.weather.current;
+    },
+    showDailyForecast(state) {
+      return state.weather.forecast;
+    }
+  }
 });
